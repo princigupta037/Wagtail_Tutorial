c6b18cede13034c419c3ed508bd584410eabd5cf
Princi Gupta <princygupta037@gmail.com>
2019-12-24 15:56:04 +0530

    categories

--- blog/models.py
+++ blog/models.py
@@ -9,6 +9,31 @@ from wagtail.core.models import Page, Orderable
 from modelcluster.fields import ParentalKey
 from modelcluster.contrib.taggit import ClusterTaggableManager
 from taggit.models import TaggedItemBase
+from wagtail.snippets.models import register_snippet
+from django import forms
+from modelcluster.fields import ParentalKey, ParentalManyToManyField
+
+
+
+
+@register_snippet
+class BlogCategory(models.Model):
+    name = models.CharField(max_length=255)
+    icon = models.ForeignKey(
+        'wagtailimages.Image', null=True, blank=True,
+        on_delete=models.SET_NULL, related_name='+'
+    )
+
+    panels = [
+        FieldPanel('name'),
+        ImageChooserPanel('icon'),
+    ]
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'blog categories'
 
 class BlogIndexPage(Page):
     intro = RichTextField(blank=True)
@@ -38,6 +63,7 @@ class BlogPage(Page):
     intro = models.CharField(max_length=250)
     body = RichTextField(blank=True)
     tags = ClusterTaggableManager(through=BlogPageTag, blank=True)
+    categories = ParentalManyToManyField('blog.BlogCategory', blank=True)
 
     # ... (Keep the main_image method and search_fields definition)
 
@@ -45,13 +71,12 @@ class BlogPage(Page):
         MultiFieldPanel([
             FieldPanel('date'),
             FieldPanel('tags'),
+            FieldPanel('categories', widget=forms.CheckboxSelectMultiple),
         ], heading="Blog information"),
         FieldPanel('intro'),
         FieldPanel('body'),
         InlinePanel('gallery_images', label="Gallery images"),
     ]
-
-
 class BlogPageGalleryImage(Orderable):
     page = ParentalKey(BlogPage, on_delete=models.CASCADE, related_name='gallery_images')
     image = models.ForeignKey(
@@ -63,3 +88,16 @@ class BlogPageGalleryImage(Orderable):
         ImageChooserPanel('image'),
         FieldPanel('caption'),
     ]
+
+    class BlogTagIndexPage(Page):
+
+        def get_context(self, request):
+
+            # Filter by tag
+            tag = request.GET.get('tag')
+            blogpages = BlogPage.objects.filter(tags__name=tag)
+
+            # Update template context
+            context = super().get_context(request)
+            context['blogpages'] = blogpages
+            return context
